e3a0f2fe5399913eabc05b1ff7f0d84e5e272cfa
holim0 <holim1226@gmail.com>
2024-07-08 23:04:57 +0900

    캐시 파일 제거

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Suspense } from "react";
+import ServerComponent from "./Component.server";
+import ImageUpload from "./ImageUpload"
+import WebpComponent from "./WebpComponent";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>dasda</h1>
+      <Suspense fallback={<div>loading...</div>}>
+        <ServerComponent />
+      </Suspense>
+      <ImageUpload />
+      <WebpComponent />
+    </div>
+  );
+}
